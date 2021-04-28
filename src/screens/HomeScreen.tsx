b4275694dab710a5164e537c5ef9a1371a4ce7eb
diego <diego.y8@gmail.com>
2021-04-27 20:06:07 -0400

    GradiantAwesome

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,6 +4,7 @@ import { FlatList } from 'react-native-gesture-handler';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import Carousel from 'react-native-snap-carousel';
+import { GradientBackground } from '../components/GradientBackground';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { MoviePoster } from '../components/MoviePoster';
 import {useMovies} from '../hooks/useMovies';
@@ -24,48 +25,42 @@ export const HomeScreen = () => {
   }
 
     return (
+  <GradientBackground>
 
+    
     <ScrollView>
 
-      <View style={{marginTop:20}}>
-        <View style={{height:440}}>
-         
-         <Carousel 
-         data={nowPlaying!}
-         renderItem={ ( {item }: any ) => <MoviePoster movie={item}/>}
-         sliderWidth={windowWidth}
-         itemWidth={300}
-         inactiveSlideOpacity={0.9}
-         />
-        </View>
-
-        <View>
-        {/* Peliculas Popular */}
-          <HorizontalSlider 
-          title="En Populares"
-          movies={popular!}
+        <View style={{marginTop:20}}>
+          <View style={{height:440}}>
+          
+          <Carousel 
+          data={nowPlaying!}
+          renderItem={ ( {item }: any ) => <MoviePoster movie={item}/>}
+          sliderWidth={windowWidth}
+          itemWidth={300}
+          inactiveSlideOpacity={0.9}
           />
-          <HorizontalSlider 
-          title="Top Rated"
-          movies={topRated!}
-          />
+          </View>
 
-          <HorizontalSlider 
-          title="Upcoming"
-          movies={upcoming!}
-          /> 
-        </View>
-        </View>
+          <View>
+          {/* Peliculas Popular */}
+            <HorizontalSlider 
+            title="En Populares"
+            movies={popular!}
+            />
+            <HorizontalSlider 
+            title="Top Rated"
+            movies={topRated!}
+            />
+
+            <HorizontalSlider 
+            title="Upcoming"
+            movies={upcoming!}
+            /> 
+          </View>
+          </View>
       </ScrollView>
-
-
-     
-
-        
-
-        
-
-         
-
+  </GradientBackground>
+  
     )
 }
